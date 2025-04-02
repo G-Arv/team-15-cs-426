4b5262de0f56c8541fc4a6422cadf57d301ef929
Kailin Luo <kailinluo@umass.edu>
2025-04-02 14:06:35 -0400

    fixed imports in Profile.tsx

--- frontend/src/profile/Profile.tsx
+++ frontend/src/profile/Profile.tsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
 import NavBar from "../components/NavBar";
 import SearchBar from "../components/SearchBar";
 
 export function Profile () {
-    const [searchTerm, setSearchTerm] = useState("");
 
     return (
     <>
         <header>Profile Page</header>
         <NavBar />
-        <SearchBar value={searchTerm} onChange={setSearchTerm} placeholder="Search items..." />
+        <SearchBar />
     </>
     );
 }
