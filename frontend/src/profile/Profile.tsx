ada8a9409d4ff24f8f57b6c10b2d5dbfc0702958
Kailin Luo <kailinluo@umass.edu>
2025-04-04 00:10:12 -0400

    added search bar and document cards to profile page

--- frontend/src/profile/Profile.tsx
+++ frontend/src/profile/Profile.tsx
@@ -1,13 +1,17 @@
 import { ProfileCard } from "../components/ProfileCard.tsx";
 import { DocumentCards } from "@/components/DocumentCard.tsx";
+import SearchBar from "../components/SearchBar.tsx";
+import { Button } from "@/components/ui/button.tsx";
 
 export function Profile () {
 
     return (
     <>
-        <div className="flex flex-col items-center min-h-screen p-4">
+        <div className="flex flex-col items-center min-h-screen p-4 space-y-8">
                 <header className="text-2xl font-bold mb-4">Profile Page</header>
                 <ProfileCard />
+                <SearchBar/>
+                <Button type="submit">Upload Document</Button>
                 <DocumentCards/>
         </div>
     </> 
