10748cf1db3070f96220980d0e4ef4d1e221ce86
Kailin Luo <99153866+KailinLuo@users.noreply.github.com>
2025-04-07 15:54:59 -0400

    Revert "Fixed import issues to most recent version, current final version of …" (#40)
    This reverts commit 80fb9f6e961f6c49397a7ab4167614feaa65bea2.

--- frontend/src/journal/Journal.tsx
+++ frontend/src/journal/Journal.tsx
@@ -1,15 +1,35 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+// import { NavSidebar } from "@/components/NavBar/NavSideBar";
 import "./Journal.css"
 import Layout from "@/components/NavBar/NavSidebarLayout";
 
-export function Journal () {
+export function Journal ({ children }: { children: React.ReactNode }) {
     return (
         <>
             <Layout>
             <div className="mt-3 ml-2">
-                <header>Journal Page</header>
+                {/* <header>Journal Page</header> */}
             </div>
             </Layout>
+
+            <header>Journal Page</header>
         </>
+
+    // <div className="journalContainer">  
+    //     <div>
+    //         <SidebarProvider>
+    //         <NavSidebar />
+    //         <main>
+    //             <SidebarTrigger />
+    //             {children}
+    //         </main>
+    //         </SidebarProvider>
+    //     </div>
+
+    //     <div className="journalHeaders">
+    //         <header>Journal Page</header>
+    //     </div>
+    // </div>
     );
 }
 
--- frontend/src/settings/Settings.tsx
+++ frontend/src/settings/Settings.tsx
@@ -1,13 +1,30 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+// import { NavSidebar } from "@/components/NavBar/NavSideBar";
 import "./Settings.css"
 import Layout from "@/components/NavBar/NavSidebarLayout";
 
-export function Settings () {
+export function Settings ({ children }: { children: React.ReactNode }) {
     return (
         <Layout>
         <div className="mt-3 ml-2">
-            <header>Settings Page</header>
+        <header>Settings Page</header>
         </div>
         </Layout>
+    // <div className="settingsContainer">
+    //     <div>
+    //         <SidebarProvider>
+    //         <NavSidebar />
+    //         <main>
+    //             <SidebarTrigger />
+    //             {children}
+    //         </main>
+    //         </SidebarProvider>
+    //     </div>
+
+    //     <div className="settingsHeaders">
+    //         <header>Settings Page</header>
+    //     </div>
+    // </div>
     );
 }
 
