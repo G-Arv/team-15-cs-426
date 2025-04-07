80fb9f6e961f6c49397a7ab4167614feaa65bea2
G-Arv <106117841+G-Arv@users.noreply.github.com>
2025-04-07 14:01:34 -0400

    Fixed import issues to most recent version, current final version of schedule page for Milestone 1 (#39)

--- frontend/src/settings/Settings.tsx
+++ frontend/src/settings/Settings.tsx
@@ -1,30 +1,13 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-// import { NavSidebar } from "@/components/NavBar/NavSideBar";
 import "./Settings.css"
 import Layout from "@/components/NavBar/NavSidebarLayout";
 
-export function Settings ({ children }: { children: React.ReactNode }) {
+export function Settings () {
     return (
         <Layout>
         <div className="mt-3 ml-2">
-        <header>Settings Page</header>
+            <header>Settings Page</header>
         </div>
         </Layout>
-    // <div className="settingsContainer">
-    //     <div>
-    //         <SidebarProvider>
-    //         <NavSidebar />
-    //         <main>
-    //             <SidebarTrigger />
-    //             {children}
-    //         </main>
-    //         </SidebarProvider>
-    //     </div>
-
-    //     <div className="settingsHeaders">
-    //         <header>Settings Page</header>
-    //     </div>
-    // </div>
     );
 }
 
